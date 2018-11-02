69fcc5080cedb9b73d80124d7b5f3e5999cd71f1
Nikita Zharkovsky <zharkovsky@angelsit.ru>
2018-11-02 11:57:01 +0300

    Lab3

--- InformationSecurity/MainWindow.xaml.cs
+++ InformationSecurity/MainWindow.xaml.cs
@@ -14,62 +14,28 @@ namespace InformationSecurity
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int[] rowPermutation;
-        private int[] colPermutation;
-        private int row, col;
+        int N = 0;
+
+        char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' };
 
         public MainWindow()
         {
             InitializeComponent();
+            N = characters.Length;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!CheckKeys(Input.Text, out row, out col)) return;
-            string[,] matr = new string[row, col];
-            rowPermutation = new int[row];
-            colPermutation = new int[col];
-            int index = 0;
-            RowKey.Text.Split(' ').ToList().ForEach(_ => rowPermutation[index++] = Convert.ToInt32(_));
-            index = 0;
-            ColumnKey.Text.Split(' ').ToList().ForEach(_ => colPermutation[index++] = Convert.ToInt32(_));
-            var result = Encode(Input.Text, matr);
-            index = 1;
-            Output.Text = "";
-            if (!string.IsNullOrWhiteSpace(Directory.Text))
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(Directory.Text);
-                foreach (FileInfo file in dirInfo.GetFiles())
-                {
-                    file.Delete();
-                }
-            }
-            result.ToList().ForEach(_ =>
-            {
-                Output.Text += $"Блок {index}: {_} \n";
-                if(!string.IsNullOrWhiteSpace(Directory.Text))
-                {
-                    var array = Encoding.UTF8.GetBytes(_);
-                    using (var file = File.Create(Path.Combine(Directory.Text, index + ".txt")))
-                    {
-                        file.Write(array, 0, array.Length);
-                    }
-                }
-                index++;
-            });
+            Output.Text = Encode(Input.Text, Key.Text);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if (!CheckKeys(Input.Text, out row, out col)) return;
-            string[,] matr = new string[row, col];
-            rowPermutation = new int[row];
-            colPermutation = new int[col];
-            int index = 0;
-            RowKey.Text.Split(' ').ToList().ForEach(_ => rowPermutation[index++] = Convert.ToInt32(_));
-            index = 0;
-            ColumnKey.Text.Split(' ').ToList().ForEach(_ => colPermutation[index++] = Convert.ToInt32(_));
-            Input.Text = Decode(Input.Text, matr);
+            Input.Text = Decode(Output.Text, Key.Text);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -81,131 +47,55 @@ namespace InformationSecurity
             }
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private string Encode(string input, string keyword)
         {
-            var fbd = new WPFFolderBrowserDialog();
-            if ((fbd.ShowDialog() ?? false) && !string.IsNullOrWhiteSpace(fbd.FileName))
-            {
-                Directory.Text = fbd.FileName;
-            }
-        }
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
 
-        private bool CheckKeys(string inputString, out int row, out int col)
-        {
-            bool result = true;
-            var set = new HashSet<int>();
-            try
-            {
-                RowKey.Text.Split(' ').ToList().ForEach(_ =>
-                {
-                    set.Add(Convert.ToInt32(_));
-                });
-                int rows = set.Count;
-                set.ToList().ForEach(_ => result = result && 0 < _ && _ <= rows);
-                set.Clear();
-                ColumnKey.Text.Split(' ').ToList().ForEach(_ =>
-                {
-                    if (set.Contains(Convert.ToInt32(_))) result = false;
-                    set.Add(Convert.ToInt32(_));
-                });
-                int cols = set.Count;
-                set.ToList().ForEach(_ => result = result && 0 < _ && _ <= cols);
+            string result = "";
 
-                row = RowKey.Text.Split(' ').Length;
-                col = cols;
+            int keyword_index = 0;
 
-                result = result && (rows * cols == Convert.ToInt32(NumBlocks.Text) && row * col >= inputString.Length);
-            }
-            catch (Exception ex)
+            foreach (char symbol in input)
             {
-                row = col = 0;
-                result = false;
+                int c = (Array.IndexOf(characters, symbol) +
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[c];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
             }
-            if (!result)
-            {
-                Error.Text = "Ошибка ключа. Проверьте ключ";
-                Output.Text = "";
-            }
-            else
-                Error.Text = "";
+
             return result;
         }
-
-        private string[] Encode(string str, string[,] matr)
-        {
-            var codeStr = new string[Convert.ToInt32(NumBlocks.Text)];
-            int strIter = 0, K;
-
-            //write
-            for (int r = 0; r < row; r++)
-            {
-                for (int c = 0; c < col; c++)
-                {
-                    if (strIter < str.Length)
-                    { //если еще не всю строку переписали
-                        matr[r, c] = (str[strIter]).ToString();
-                        strIter++;
-                    }
-                    else
-                    {
-                        matr[r, c] = " ";
-                    }
-                }
-            }
-
-            //read
-            for (int c = 0; c < col; c++)
-            {
-                for (int r = 0; r < row; r++)
-                {
-                    K = col * (rowPermutation[r] - 1) + colPermutation[c];
-                    codeStr[K - 1] += matr[r, c];
-                }
-            }
-
-            return codeStr;
-        }
-
         
-
-        private string Decode(string str, string[,] matr)
+        //расшифровать
+        private string Decode(string input, string keyword)
         {
-            string decodeStr = "";
-            var codeStr = new string[Convert.ToInt32(NumBlocks.Text)];
-            int i = 0, strIter = 0, K;
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
 
-            var directory = Directory.Text;
-            if (string.IsNullOrWhiteSpace(directory)) return null;
+            string result = "";
 
-            DirectoryInfo dirInfo = new DirectoryInfo(directory);
-            foreach (FileInfo file in dirInfo.GetFiles())
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
             {
-                codeStr[i++] = File.ReadAllText(file.FullName);
-            }
-            
+                int p = (Array.IndexOf(characters, symbol) + N -
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
 
-            //read
-            for (int c = 0; c < col; c++)
-            {
-                for (int r = 0; r < row; r++)
-                {
-                    K = col * (rowPermutation[r] - 1) + colPermutation[c];
-                    matr[r, c] = codeStr[K - 1].FirstOrDefault().ToString();
-                    if(!string.IsNullOrWhiteSpace(codeStr[K-1]))
-                        codeStr[K - 1] = codeStr[K - 1].Remove(0, 1);
-                }
+                result += characters[p];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
             }
 
-            //write
-            for (int r = 0; r < row; r++)
-            {
-                for (int c = 0; c < col; c++)
-                {
-                   decodeStr += matr[r, c];
-                }
-            }
-
-            return decodeStr;
+            return result;
         }
 
     }
