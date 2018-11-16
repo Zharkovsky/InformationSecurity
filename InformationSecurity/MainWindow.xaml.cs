89a34081878135751cddde11cc4010c8ce198dcd
Nikita Zharkovsky <zharkovsky@angelsit.ru>
2018-11-16 09:17:28 +0300

    Lab4 Hash gost

--- InformationSecurity/MainWindow.xaml.cs
+++ InformationSecurity/MainWindow.xaml.cs
@@ -14,89 +14,22 @@ namespace InformationSecurity
     /// </summary>
     public partial class MainWindow : Window
     {
-        int N = 0;
-
-        char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
-                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
-                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
-                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
-                                                '8', '9', '0' };
+        GOST G = new GOST(256);
+        GOST G512 = new GOST(512);
 
         public MainWindow()
         {
             InitializeComponent();
-            N = characters.Length;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Output.Text = Encode(Input.Text, Key.Text);
+            var message = Encoding.UTF8.GetBytes(Input.Text);
+            byte[] res = G.GetHash(message);
+            byte[] res2 = G512.GetHash(message);
+            string h256 = BitConverter.ToString(res);
+            string h512 = BitConverter.ToString(res2);
+            Output.Text = h256 + "\n" + h512;
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Input.Text = Decode(Output.Text, Key.Text);
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            {
-                Input.Text = Encoding.UTF8.GetString(File.ReadAllBytes(openFileDialog.FileName));
-            }
-        }
-
-        private string Encode(string input, string keyword)
-        {
-            input = input.ToUpper();
-            keyword = keyword.ToUpper();
-
-            string result = "";
-
-            int keyword_index = 0;
-
-            foreach (char symbol in input)
-            {
-                int c = (Array.IndexOf(characters, symbol) +
-                    Array.IndexOf(characters, keyword[keyword_index])) % N;
-
-                result += characters[c];
-
-                keyword_index++;
-
-                if ((keyword_index + 1) == keyword.Length)
-                    keyword_index = 0;
-            }
-
-            return result;
-        }
-        
-        //расшифровать
-        private string Decode(string input, string keyword)
-        {
-            input = input.ToUpper();
-            keyword = keyword.ToUpper();
-
-            string result = "";
-
-            int keyword_index = 0;
-
-            foreach (char symbol in input)
-            {
-                int p = (Array.IndexOf(characters, symbol) + N -
-                    Array.IndexOf(characters, keyword[keyword_index])) % N;
-
-                result += characters[p];
-
-                keyword_index++;
-
-                if ((keyword_index + 1) == keyword.Length)
-                    keyword_index = 0;
-            }
-
-            return result;
-        }
-
     }
 }
