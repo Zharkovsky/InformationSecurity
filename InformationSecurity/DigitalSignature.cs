e817a9c9093ac44cda8abe031d94eb44078e1f0c
Nikita Zharkovsky <zharkovsky@angelsit.ru>
2018-11-23 09:46:22 +0300

    Lab6

---
+++ InformationSecurity/DigitalSignature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationSecurity
+{
+    public class DigitalSignature
+    {
+        public RSAParameters PublicKey { get; set; }
+        public RSAParameters PrivateKey { get; set; }
+
+        public void AssignNewKey()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                PublicKey = rsa.ExportParameters(false);
+                PrivateKey = rsa.ExportParameters(true);
+            }
+        }
+        public byte[] SignData(byte[] hashOfDataToSign)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(PrivateKey);
+
+                var rsaFormatter = new RSAPKCS1SignatureFormatter(rsa);
+                rsaFormatter.SetHashAlgorithm("SHA256");
+                return rsaFormatter.CreateSignature(hashOfDataToSign);
+            }
+        }
+        public bool VerifySignature(byte[] hashOfDataToSign, byte[] signature)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.ImportParameters(PublicKey);
+                var rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                rsaDeformatter.SetHashAlgorithm("SHA256");
+                return rsaDeformatter.VerifySignature(hashOfDataToSign, signature);
+            }
+        }
+    }
+}
--- InformationSecurity/MainWindow.xaml.cs
+++ InformationSecurity/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,7 +16,9 @@ namespace InformationSecurity
     /// </summary>
     public partial class MainWindow : Window
     {
-        byte[] encryptedRSA;
+        DigitalSignature ds = new DigitalSignature();
+        byte[] signature;
+        byte[] hash;
 
         public MainWindow()
         {
@@ -26,46 +29,136 @@ namespace InformationSecurity
         {
             try
             {
-                //Generate keys
-                RSACryptoServiceProvider rsaGenKeys = new RSACryptoServiceProvider();
-                string privateXml = rsaGenKeys.ToXmlString(true);
-                string publicXml = rsaGenKeys.ToXmlString(false);
-                Public.Text = publicXml;
-                Private.Text = privateXml;
-                Error.Text = "";
+                ds.AssignNewKey();
+                Public.Text = JsonConvert.SerializeObject(ds.PublicKey);
+                Private.Text = JsonConvert.SerializeObject(ds.PrivateKey);
             }
             catch { Error.Text = "Ошибка"; }
         }
 
-        private void Encrypt(object sender, RoutedEventArgs e)
+        private void SignData(object sender, RoutedEventArgs e)
         {
             try
             {
                 //Encode with public key
-                byte[] toEncryptData = Encoding.ASCII.GetBytes(Input.Text);
-                RSACryptoServiceProvider rsaPublic = new RSACryptoServiceProvider();
-                rsaPublic.FromXmlString(Public.Text);
-                encryptedRSA = rsaPublic.Encrypt(toEncryptData, false);
-                string EncryptedResult = Encoding.Default.GetString(encryptedRSA);
+                HashAlgorithm algorithm = SHA256.Create();
+                hash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(Input.Text));
+                signature = ds.SignData(hash);
+                string EncryptedResult = Encoding.Default.GetString(signature);
                 Output.Text = EncryptedResult;
                 Error.Text = "";
             }
-            catch { Error.Text = "Ошибка"; }
+            catch (Exception ex) { Error.Text = "Ошибка"; }
         }
 
-        private void Decrypt(object sender, RoutedEventArgs e)
+        private void Verify(object sender, RoutedEventArgs e)
         {
             try
             {
                 //Decode with private key
-                var rsaPrivate = new RSACryptoServiceProvider();
-                rsaPrivate.FromXmlString(Private.Text);
-                byte[] decryptedRSA = rsaPrivate.Decrypt(encryptedRSA, false);
-                string originalResult = Encoding.Default.GetString(decryptedRSA);
-                Output.Text = originalResult;
+                HashAlgorithm algorithm = SHA256.Create();
+                var hash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(Input.Text));
+                Result.Text = ds.VerifySignature(hash, signature) ? "Подпись подтверждена" : "Подпись не подтверждена";
                 Error.Text = "";
             }
-            catch { Error.Text = "Ошибка"; }
+            catch (Exception ex) { Error.Text = "Ошибка"; }
+        }
+    }
+
+
+    public sealed class Base32
+    {
+
+        // the valid chars for the encoding
+        private static string ValidChars = "QAZ2WSX3" + "EDC4RFV5" + "TGB6YHN7" + "UJM8K9LP";
+
+        /// <summary>
+        /// Converts an array of bytes to a Base32-k string.
+        /// </summary>
+        public static string ToBase32String(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();         // holds the base32 chars
+            byte index;
+            int hi = 5;
+            int currentByte = 0;
+
+            while (currentByte < bytes.Length)
+            {
+                // do we need to use the next byte?
+                if (hi > 8)
+                {
+                    // get the last piece from the current byte, shift it to the right
+                    // and increment the byte counter
+                    index = (byte)(bytes[currentByte++] >> (hi - 5));
+                    if (currentByte != bytes.Length)
+                    {
+                        // if we are not at the end, get the first piece from
+                        // the next byte, clear it and shift it to the left
+                        index = (byte)(((byte)(bytes[currentByte] << (16 - hi)) >> 3) | index);
+                    }
+
+                    hi -= 3;
+                }
+                else if (hi == 8)
+                {
+                    index = (byte)(bytes[currentByte++] >> 3);
+                    hi -= 3;
+                }
+                else
+                {
+
+                    // simply get the stuff from the current byte
+                    index = (byte)((byte)(bytes[currentByte] << (8 - hi)) >> 3);
+                    hi += 5;
+                }
+
+                sb.Append(ValidChars[index]);
+            }
+
+            return sb.ToString();
+        }
+
+
+        /// <summary>
+        /// Converts a Base32-k string into an array of bytes.
+        /// </summary>
+        /// <exception cref="System.ArgumentException">
+        /// Input string <paramref name="s">s</paramref> contains invalid Base32-k characters.
+        /// </exception>
+        public static byte[] FromBase32String(string str)
+        {
+            int numBytes = str.Length * 5 / 8;
+            byte[] bytes = new Byte[numBytes];
+
+            // all UPPERCASE chars
+            str = str.ToUpper();
+
+            int bit_buffer;
+            int currentCharIndex;
+            int bits_in_buffer;
+
+            if (str.Length < 3)
+            {
+                bytes[0] = (byte)(ValidChars.IndexOf(str[0]) | ValidChars.IndexOf(str[1]) << 5);
+                return bytes;
+            }
+
+            bit_buffer = (ValidChars.IndexOf(str[0]) | ValidChars.IndexOf(str[1]) << 5);
+            bits_in_buffer = 10;
+            currentCharIndex = 2;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = (byte)bit_buffer;
+                bit_buffer >>= 8;
+                bits_in_buffer -= 8;
+                while (bits_in_buffer < 8 && currentCharIndex < str.Length)
+                {
+                    bit_buffer |= ValidChars.IndexOf(str[currentCharIndex++]) << bits_in_buffer;
+                    bits_in_buffer += 5;
+                }
+            }
+
+            return bytes;
         }
     }
 }
