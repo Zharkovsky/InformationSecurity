13493d670246a25621e7ab93d96591c43a111cf4
Nikita Zharkovsky <zharkovsky@angelsit.ru>
2018-11-23 08:53:33 +0300

    Lab5

--- InformationSecurity/MainWindow.xaml.cs
+++ InformationSecurity/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using WPFFolderBrowser;
@@ -14,22 +15,57 @@ namespace InformationSecurity
     /// </summary>
     public partial class MainWindow : Window
     {
-        GOST G = new GOST(256);
-        GOST G512 = new GOST(512);
+        byte[] encryptedRSA;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void GenerateKeys(object sender, RoutedEventArgs e)
         {
-            var message = Encoding.UTF8.GetBytes(Input.Text);
-            byte[] res = G.GetHash(message);
-            byte[] res2 = G512.GetHash(message);
-            string h256 = BitConverter.ToString(res);
-            string h512 = BitConverter.ToString(res2);
-            Output.Text = h256 + "\n" + h512;
+            try
+            {
+                //Generate keys
+                RSACryptoServiceProvider rsaGenKeys = new RSACryptoServiceProvider();
+                string privateXml = rsaGenKeys.ToXmlString(true);
+                string publicXml = rsaGenKeys.ToXmlString(false);
+                Public.Text = publicXml;
+                Private.Text = privateXml;
+                Error.Text = "";
+            }
+            catch { Error.Text = "Ошибка"; }
+        }
+
+        private void Encrypt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Encode with public key
+                byte[] toEncryptData = Encoding.ASCII.GetBytes(Input.Text);
+                RSACryptoServiceProvider rsaPublic = new RSACryptoServiceProvider();
+                rsaPublic.FromXmlString(Public.Text);
+                encryptedRSA = rsaPublic.Encrypt(toEncryptData, false);
+                string EncryptedResult = Encoding.Default.GetString(encryptedRSA);
+                Output.Text = EncryptedResult;
+                Error.Text = "";
+            }
+            catch { Error.Text = "Ошибка"; }
+        }
+
+        private void Decrypt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Decode with private key
+                var rsaPrivate = new RSACryptoServiceProvider();
+                rsaPrivate.FromXmlString(Private.Text);
+                byte[] decryptedRSA = rsaPrivate.Decrypt(encryptedRSA, false);
+                string originalResult = Encoding.Default.GetString(decryptedRSA);
+                Output.Text = originalResult;
+                Error.Text = "";
+            }
+            catch { Error.Text = "Ошибка"; }
         }
     }
 }
